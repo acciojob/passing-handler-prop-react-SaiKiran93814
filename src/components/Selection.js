14592afaced2f1814f49f93547337df80c426190
SaiKiran93814 <teppali123@gmail.com>
2025-06-24 11:48:45 +0530

    Completed Assignment

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,31 @@
+import React, { useState } from "react";
 
+const Selection = ({ nextBackground }) => {
+  const [background, setBackground] = useState({ background: "" });
+  const [previewBackground, setPreviewBackground] = useState(null);
+
+  const applyPreview = () => {
+    setPreviewBackground(nextBackground);
+  };
+
+  const clearPreview = () => {
+    setPreviewBackground(null);
+  };
+
+  const applyColor = () => {
+    setBackground(nextBackground);
+    clearPreview();
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={previewBackground || background}
+      onMouseEnter={applyPreview}
+      onMouseLeave={clearPreview}
+      onClick={applyColor}
+    />
+  );
+};
+
+export default Selection;
