4ee2424bc9733fa6ca83439204ec14d259c7ca77
SaiKiran93814 <teppali123@gmail.com>
2025-07-23 22:40:21 +0530

    styles changed

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import '../styles/Child.css'
 
 const Selection = ({ nextBackground, background, index, applyBackground }) => {
   const [previewBackground, setPreviewBackground] = useState(null);
